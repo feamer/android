435f310dcb0869109ce3c92e56d693b6cf55f088
Sebastian <s.harner@web.de>
2018-06-02 23:54:23 +0200

    Update

---
+++ app/src/main/java/de/feamer/MainActivity.kt
@@ -0,0 +1,17 @@
+package de.feamer
+
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val vm by lazy { ViewModelProviders.of(this).get(MainViewModel::class.java) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+}
---
+++ app/src/main/java/de/feamer/MainViewModel.kt
@@ -0,0 +1,6 @@
+package de.feamer
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+}
